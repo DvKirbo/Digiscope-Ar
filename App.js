b32c94036a32123ae1cb1e6e45d7b225fe9e5d38
DvKirbo <cuetosalazarsebas@gmail.com>
2024-10-29 22:08:42 -0500

    tests

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const HelloWorldSceneAR = () => {
 
   function onInitialized(state, reason) {
     if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      setText("Hola kelly!");
+      setText("Hola wasa!");
     } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
       // Handle loss of tracking
     }
@@ -27,89 +27,102 @@ const HelloWorldSceneAR = () => {
 
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
-      {/* <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      /> */}
+      {/* Text to show whether or not the AR system has initialized yet, see ViroARScene's onTrackingInitialized*/}
+      <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
 
+<ViroBox position={[0, -.5, -1]}
+  animation={{name: "rotate", run: true, loop: true}}
+  scale={[.3, .3, .1]} materials={["grid"]} />
 
+<ViroAmbientLight color={"#aaaaaa"} influenceBitMask={1} />
 
-<ViroImage 
-  source={require('./assets/draco.png')}
-  position={[0, 0, -1]}
-  scale={[.5, .5, .5]}
-/>
+<ViroSpotLight
+    innerAngle={5}
+    outerAngle={90}
+    direction={[0,-1,-.2]}
+    position={[0, 3, 1]}
+    color="#aaaaaa"
+    castsShadow={true}
+    />
 
-
+{/* Node that contains a light, an object and a surface to catch its shadow
+    notice that the dragType is "FixedToWorld" so the object can be dragged
+    along real world surfaces and points. */}
 <ViroNode position={[-.5, -.5, -.5]} dragType="FixedToWorld" onDrag={()=>{}} >
 
-{/* Spotlight to cast light on the object and a shadow on the surface, see
-    the Viro documentation for more info on lights & shadows */}
-<ViroSpotLight
-  innerAngle={5}
-  outerAngle={45}
-  direction={[0,-1,-.2]}
-  position={[0, 3, 0]}
-  color="#ffffff"
-  castsShadow={true}
-  influenceBitMask={2}
-  shadowMapSize={2048}
-  shadowNearZ={2}
-  shadowFarZ={5}
-  shadowOpacity={.7} />
+  {/* Spotlight to cast light on the object and a shadow on the surface, see
+      the Viro documentation for more info on lights & shadows */}
+  <ViroSpotLight
+    innerAngle={5}
+    outerAngle={45}
+    direction={[0,-1,-.2]}
+    position={[0, 3, 0]}
+    color="#ffffff"
+    castsShadow={true}
+    influenceBitMask={2}
+    shadowMapSize={2048}
+    shadowNearZ={2}
+    shadowFarZ={5}
+    shadowOpacity={.7} />
 
+  <Viro3DObject
+      source={require('./res/emoji_smile/emoji_smile.vrx')}
+      position={[0, .2, 0]}
+      scale={[.2, .2, .2]}
+      type="VRX"
+    lightReceivingBitMask={3}
+    shadowCastingBitMask={2}
+    transformBehaviors={['billboardY']}
+    resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
+               require('./res/emoji_smile/emoji_smile_specular.png'),
+               require('./res/emoji_smile/emoji_smile_normal.png')]}/>
 
-<Viro3DObject
-              source={require('./res/emoji_smile/emoji_smile.vrx')}
-              position={[0, .2, 0]}
-              scale={[.2, .2, .2]}
-              type="VRX"
-            lightReceivingBitMask={3}
-            shadowCastingBitMask={2}
-            transformBehaviors={['billboardY']}
-            resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
-                       require('./res/emoji_smile/emoji_smile_specular.png'),
-                       require('./res/emoji_smile/emoji_smile_normal.png')]}/>
-
-<ViroQuad
-  rotation={[-90,0,0]}
-  width={.5} height={.5}
-  arShadowReceiver={true}
-  lightReceivingBitMask={2} />
+  <ViroQuad
+    rotation={[-90,0,0]}
+    width={.5} height={.5}
+    arShadowReceiver={true}
+    lightReceivingBitMask={2} />
 
 </ViroNode>
 
 {/* Node that contains a light, an object and a surface to catch its shadow
-notice that the dragType is "FixedToWorld" so the object can be dragged
-along real world surfaces and points. */}
-<ViroNode position={[.5,-.5,-.5]} dragType="FixedDistanceOrigin" onDrag={()=>{}} >
+  notice that the dragType is "FixedToWorld" so the object can be dragged
+  along real world surfaces and points. */}
+<ViroNode position={[.5,-.5,-.5]} dragType="FixedToWorld" onDrag={()=>{}} >
 
-{/* Spotlight to cast light on the object and a shadow on the surface, see
-    the Viro documentation for more info on lights & shadows */}
-{/* <ViroSpotLight
-  innerAngle={5}
-  outerAngle={45}
-  direction={[0,-1,-.2]}
-  position={[0, 3, 0]}
-  color="#ffffff"
-  castsShadow={true}
-  influenceBitMask={4}
-  shadowMapSize={2048}
-  shadowNearZ={2}
-  shadowFarZ={5}
-  shadowOpacity={.7} />
- */}
-{/* <ViroQuad
-  rotation={[-90,0,0]}
-  width={.5} height={.5}
-  arShadowReceiver={true}
-  lightReceivingBitMask={4} /> */}
+  {/* Spotlight to cast light on the object and a shadow on the surface, see
+      the Viro documentation for more info on lights & shadows */}
+  <ViroSpotLight
+    innerAngle={5}
+    outerAngle={45}
+    direction={[0,-1,-.2]}
+    position={[0, 3, 0]}
+    color="#ffffff"
+    castsShadow={true}
+    influenceBitMask={4}
+    shadowMapSize={2048}
+    shadowNearZ={2}
+    shadowFarZ={5}
+    shadowOpacity={.7} />
 
-</ViroNode>
+  <Viro3DObject
+    source={require('./res/object_soccerball/object_soccer_ball.vrx')}
+    position={[0, .15, 0]}
+    scale={[.3, .3, .3]}
+    type="VRX"
+    lightReceivingBitMask={5}
+    shadowCastingBitMask={4}
+    transformBehaviors={['billboardY']}
+    resources={[require('./res/object_soccerball/object_soccer_ball_diffuse.png'),
+               require('./res/object_soccerball/object_soccer_ball_normal.png'),
+               require('./res/object_soccerball/object_soccer_ball_specular.png')]}/>
+  <ViroQuad
+    rotation={[-90,0,0]}
+    width={.5} height={.5}
+    arShadowReceiver={true}
+    lightReceivingBitMask={4} />
 
-
+    </ViroNode>
     </ViroARScene>
   );
 };
